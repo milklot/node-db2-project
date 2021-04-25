9b3c946cfd667cac2fa5689981def8db142f2ac1
milklot <vbiostflex@gmail.com>
2021-04-25 16:57:15 -0400

    mvp

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,23 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('cars');
 }
 
-const getById = () => {
+const getById = (id) => {
   // DO YOUR MAGIC
+  return db('cars').where({id}).first();
 }
 
-const create = () => {
+const create = (newCar) => {
   // DO YOUR MAGIC
+  const [id] =  db('cars').insert(newCar)
+    return getById(id)
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create
 }
